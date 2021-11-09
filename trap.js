969d7c5b36e6bd7351f23d481b9f0937f4ef2868
SuicaDavid <genglanghuang@gmail.com>
2021-11-09 22:49:15 +0000

    refine the solution of trap.js

--- trap.js
+++ trap.js
@@ -3,65 +3,26 @@
  * @return {number}
  */
 let trap = function (height) {
-	let start
-	let pool = []
+	let leftMax = -1
+	let rightMax = -1
 	let volumn = 0
-	function add(a, b) {
-		return a + b
-	}
+	let left = 0
+	let right = height.length - 1
+	while (left <= right) {
+		leftMax = Math.max(leftMax, height[left])
+		rightMax = Math.max(rightMax, height[right])
 
-	function minIndex(list) {
-		if (list.length < 1) {
-			return list.length - 1
-		}
-		let min = list[0]
-		let index = 0
-		for (let i = 0; i < list.length; i++) {
-			if (list[i] < min) {
-				min = list[i]
-				index = i
-			}
-		}
-		return index
-	}
-
-	for (let i = 0; i < height.length; i++) {
-		let h = height[i]
-		if (h !== 0 && !start) {
-			start = h
-			for (let j = i + 1; j < height.length; j++) {
-				let he = height[j]
-				if (he < start) {
-					pool.push(start - he)
-					if (j === height.length - 1) {
-						let index = minIndex(pool)
-						let min = pool[index]
-						if (index > 0 && index < height.length) {
-							pool = pool
-								.slice(0, index)
-								.map((item) => item - min)
-							volumn += pool.reduce(add, 0)
-							i = i + index - 1
-						}
-						pool = []
-						start = null
-						break
-					}
-				} else {
-					volumn += pool.reduce(add, 0)
-					pool = []
-					start = null
-					i = j - 1
-					break
-				}
-			}
+		if (height[left] < height[right]) {
+			volumn += leftMax - height[left++]
+		} else {
+			volumn += rightMax - height[right--]
 		}
 	}
 	return volumn
 }
 
-// console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
-// console.log(trap([4, 2, 0, 3, 2, 5]))
-// console.log(trap([4, 2, 0, 3, 2, 5]))
+console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
+console.log(trap([4, 2, 0, 3, 2, 5]))
+console.log(trap([4, 2, 0, 3, 2, 5]))
 console.log(trap([4, 2, 3]))
 console.log(trap([5, 4, 1, 2]))
