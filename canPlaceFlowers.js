1a9778ce192c332ef850533fd1683185e77a2ed9
SuicaDavid <genglanghuang@gmail.com>
2021-12-03 17:08:26 +0000

    add canPlaceFlowers.js

--- canPlaceFlowers.js
+++ canPlaceFlowers.js
@@ -5,6 +5,7 @@
  */
 let canPlaceFlowers = function (flowerbed, n) {
 	for (let i = 0; i < flowerbed.length && n > 0; i++) {
+        console.log(flowerbed[i - 1], flowerbed[i - 1] === 0)
 		if (
 			flowerbed[i] === 0 &&
 			flowerbed[i - 1] !== 1 &&
