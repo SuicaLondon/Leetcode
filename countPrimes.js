af1446c4e71d744f0433078a2838b19b2eb68ce0
SuicaDavid <genglanghuang@gmail.com>
2021-12-05 21:22:09 +0000

    refine countPrimes.js

--- countPrimes.js
+++ countPrimes.js
@@ -3,23 +3,17 @@
  * @return {number}
  */
 let countPrimes = function (n) {
-    if (n <= 2) return 0
-    function isPrime(num) {
-        for (let i = 3; i <= Math.sqrt(num); i+=2) {
-            if (num % i === 0) return false
-        }
-        return true
-    }
-    let list = [2]
-    for (let i = 3; i < n; i+=2) {
-        if (isPrime(i)) {
-            list.push(i)
-        }
-    }
-    console.log(list)
-    return list.length
+	let grid = new Uint8Array(n),
+		counter = 0
+	for (let i = 2; i < n; i++) {
+		if (grid[i]) continue
+		counter++
+		for (let mult = i * i; mult < n; mult += i) {
+			grid[mult] = true
+		}
+	}
+	return counter
 }
-
 
 console.log(countPrimes(10))
 console.log(countPrimes(0))
