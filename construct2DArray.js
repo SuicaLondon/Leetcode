94e703569283b5c261a799e064b010ea83f822ae
SuicaDavid <suicadevinlondon@gmail.com>
2022-07-18 23:25:40 +0100

    add construct2DArray

---
+++ construct2DArray.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} original
+ * @param {number} m
+ * @param {number} n
+ * @return {number[][]}
+ */
+let construct2DArray = function (original, m, n) {
+    if (original.length !== m * n) return []
+    let list = []
+    for (let i = 0; i < m; i++) {
+        let l = []
+        for (let j = 0; j < n; j++) {
+            l.push(original.shift())
+        }
+        list.push(l)
+    }
+    return list
+}
+
+console.log(construct2DArray([1, 2, 3, 4], 2, 2))
+console.log(construct2DArray([1, 2, 3], 1, 3))
+console.log(construct2DArray([1, 2], 1, 1))
+console.log(construct2DArray([3], 1, 2))
