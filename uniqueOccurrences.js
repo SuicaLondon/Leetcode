9eea07ee78e66ed4113c738bf8cb678b6a7d20c5
SuicaDavid <suicadevinlondon@gmail.com>
2022-07-21 22:40:33 +0100

    add uniqueOccurrences

---
+++ uniqueOccurrences.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+let uniqueOccurrences = function (arr) {
+    let obj = {}
+    for (let i = 0; i < arr.length; i++) {
+        obj[arr[i]] = obj[arr[i]] ? obj[arr[i]] + 1 : 1
+    }
+    let val = Object.values(obj)
+    let unique = [...new Set(val)]
+    return val.length === unique.length
+}
+
+console.log(uniqueOccurrences([1, 2, 2, 1, 1, 3]))
+console.log(uniqueOccurrences([1, 2]))
+console.log(uniqueOccurrences([-3, 0, 1, -3, 1, 1, 1, -3, 10, 0]))
