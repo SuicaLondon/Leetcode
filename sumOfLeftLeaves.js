96900a06b54219bf39d0c17bab9218b09b5d0567
SuicaDavid <suicadevinlondon@gmail.com>
2022-07-24 23:23:36 +0100

    Submit new solution

--- sumOfLeftLeaves.js
+++ sumOfLeftLeaves.js
@@ -10,27 +10,8 @@
  * @param {TreeNode} root
  * @return {number}
  */
- let sumOfLeftLeaves = function (root) {
-    let count = 0
-    function accessChildNodeAndRecord(node, count, leaf) {
-        if (node.left === null && node.right === null) {
-            if (leaf && leaf === 'left') {
-                count += node.val
-            }
-        } else {
-            if (node.left) {
-                count = accessChildNodeAndRecord(node.left, count, 'left')
-            } 
-            if (node.right) {
-                count = accessChildNodeAndRecord(node.right, count, 'right')
-            }
-        }
-        return count
-    }
-    if (root === null) {
-        return 0
-    } else {
-        count = accessChildNodeAndRecord(root, count)
-        return count
-    }
+let sumOfLeftLeaves = function (root, isLeft) {
+    if (root === null) return 0
+    if (root.left === null && root.right === null && isLeft) return root.val
+    return sumOfLeftLeaves(root.left, true) + sumOfLeftLeaves(root.right, false)
 }
