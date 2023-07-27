9117038635064a1acd9eca515e8ea6b837dd7e19
SuicaLondon <66697085+SuicaDavid@users.noreply.github.com>
2023-07-27 15:48:25 +0100

    Create once.ts

---
+++ once.ts
@@ -0,0 +1,18 @@
+type Fn = (...args: any[]) => any
+
+function once(fn: Fn): Fn {
+    let counter = 0
+    return function (...args) {
+        if (counter > 0) return
+        counter++
+        return fn(...args)
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
