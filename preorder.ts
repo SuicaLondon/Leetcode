6e920ef44b36f4d02a619485152ac115811b252c
SuicaLondon <66697085+SuicaDavid@users.noreply.github.com>
2023-08-15 14:47:06 +0100

    Update preorder.ts

--- preorder.ts
+++ preorder.ts
@@ -12,13 +12,13 @@
 
 function preorder(root: Node | null): number[] {
     let result = []
-    const inorder = (node: Node | null) => {
+    const preorderTraversal = (node: Node | null) => {
         if (node == null) return result
         result.push(node.val)
         for (let i = 0; i < node.children.length; i++) {
-            inorder(node.children[i])
+            preorderTraversal(node.children[i])
         }
         return result
     }
-    return inorder(root)
+    return preorderTraversal(root)
 };
