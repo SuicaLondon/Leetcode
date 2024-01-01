1c9c6481400099fc564caf5e0ec7cc3e69d1d1d2
SuicaDavid <suicadevinlondon@gmail.com>
2024-01-01 18:45:05 +0000

    add NumArray.ts

---
+++ numArray.ts
@@ -0,0 +1,20 @@
+class NumArray {
+	private nums: number[]
+	constructor(nums: number[]) {
+		this.nums = nums
+	}
+
+	sumRange(left: number, right: number): number {
+		let sum = 0
+		for (let i = left; i <= right; i++) {
+			sum += this.nums[i]
+		}
+		return sum
+	}
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
