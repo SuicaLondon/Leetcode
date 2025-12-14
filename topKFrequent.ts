e48b24405629b532ee1b601f3c4ca37553b836a1
SuicaLondon <suicadevinlondon@gmail.com>
2025-12-14 23:16:54 +0800

    feat: add solution of bucket sort

--- topKFrequent.ts
+++ topKFrequent.ts
@@ -1,14 +1,32 @@
+// function topKFrequent(nums: number[], k: number): number[] {
+// 	const freqMap: Map<number, number> = new Map()
+// 	for (let i = 0; i < nums.length; i++) {
+// 		const num = nums[i]
+// 		freqMap.set(num, (freqMap.get(num) ?? 0) + 1)
+// 	}
+// 	const sorted = Array.from(freqMap.entries()).sort((a, b) => b[1] - a[1])
+// 	return sorted.slice(0, k).map((item) => item[0])
+// }
+
 function topKFrequent(nums: number[], k: number): number[] {
 	const freqMap: Map<number, number> = new Map()
 	for (let i = 0; i < nums.length; i++) {
-		const itemValue = nums[i]
-		const existedValue = freqMap.get(itemValue)
-		if (existedValue) {
-			freqMap.set(itemValue, existedValue + 1)
-		} else {
-			freqMap.set(itemValue, 1)
+		const num = nums[i]
+		freqMap.set(num, (freqMap.get(num) ?? 0) + 1)
+	}
+	const buckets: number[][] = Array.from({ length: nums.length }, () => [])
+	for (const [num, freq] of Array.from(freqMap.entries())) {
+		buckets[freq].push(num)
+	}
+
+	const result: number[] = []
+	for (let i = buckets.length - 1; i > 0 && result.length < k; i--) {
+		for (const num of buckets[i]) {
+			result.push(num)
+			if (result.length === k) break
 		}
 	}
-	const sorted = Array.from(freqMap.entries()).sort((a, b) => b[1] - a[1])
-	return sorted.slice(0, k).map((item) => item[0])
+	return result
 }
+
+console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2))
